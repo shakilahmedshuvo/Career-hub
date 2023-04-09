e842071c44194f22015c2316c0fe2dde03c4163e
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-10 04:03:51 +0600

    add font-Manrop font and some styles in header and nav

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,10 +5,10 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='flex justify-around mt-10 items-center'>
-            <h1 className='font-bold text-2xl'>Job Hub</h1>
-            <div>
+            <h1 className='font-extrabold text-3xl'>Job Hub</h1>
+            <div className='text-gray-400'>
                 <Link className='mx-6 font-medium' to='./'>Home</Link>
-                <Link className='mx-6 font-medium' to='/statistics'>Statistics</Link>
+                <Link className='mx-6 font-medium text-indigo-500' to='/statistics'>Statistics</Link>
                 <Link className='mx-6 font-medium' to='/applied'>Applied Jobs</Link>
                 <Link className='mx-6 font-medium' to='/blog'>Blog</Link>
             </div>
