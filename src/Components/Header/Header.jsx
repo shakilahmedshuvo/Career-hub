2977a2da7519b175e9317d9836da168a20ed16b2
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-09 21:47:20 +0600

    add header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div className='flex justify-around mt-10'>
+        <div className='flex justify-around mt-10 items-center'>
             <h1 className='font-bold text-2xl'>Job Hub</h1>
             <div>
                 <Link className='mx-6 font-medium' to='./'>Home</Link>
@@ -11,7 +12,7 @@ const Header = () => {
                 <Link className='mx-6 font-medium' to='/applied'>Applied Jobs</Link>
                 <Link className='mx-6 font-medium' to='/blog'>Blog</Link>
             </div>
-            <button className=''>Star Applying</button>
+            <button className='header-btn px-2'>Star Applying</button>
         </div>
     );
 };
