ae2e55e314a3fe566a512dfef67aaf9f0d4c65d0
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-04-09 22:23:25 +0600

    add header section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+            {/* banner section start */}
+            <div className='md:flex justify-around mx-60 mt-20'>
+                <div className='ml-2'>
+                    <h1 className='text-6xl font-bold'>One Step
+                        <br />
+                        Closer To Your
+                        <br />
+                        <span className='text-blue-700'>Dream Job</span>
+                    </h1>
+                    <h3 className='mt-4 text-gray-500'>Explore thousands of job opportunities with all the
+                        <br />
+                        information you need. Its your future. Come find it. Manage all
+                        <br />
+                        your job application from start to finish.</h3>
+                    <button className='mt-4 header-btn px-4'>Get Started</button>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
+                </div>
+            </div>
+            {/* banner section end */}
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,19 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Main from './Components/Main/Main';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      }
+    ]
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
